44bd1a00f1174d45b35edc2aa6be48617024b174
DNathanael19 <diego.nathanael62@aluno.ifce.edu.br>
2023-03-02 16:59:20 -0300

    .

--- sgt/ola/urls.py
+++ sgt/ola/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index2"),
-               path("<str:nome>", views.saudacao, name="saudacao")]
+urlpatterns = [path("", views.index, name="index"),
+               #path("greet", views.greet, name="greet"),
+               #path("<str:nome>", views.saudacao, name="index1"),
+               #path("<str:nome>", views.greet, name="")
+               path("", views.tia_do_zap, name="tia_do_zap")]
--- sgt/ola/views.py
+++ sgt/ola/views.py
@@ -6,5 +6,18 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def saudacao(request, nome):
-    return HttpResponse(f"<h1 style='color: red'>Olá {nome}</h1>")
+#def greet(request, nome):
+    #return render(request, 'greet.html', {'name':nome})
+
+def tia_do_zap(request, nome):
+    var = datetime.datetime.now()
+    dia = int(var.strftime("%H"))
+    boleano = bool
+    if dia > 18:
+        boleano = False
+    else: 
+        boleano = True
+    return render(request, 'tia_do_zap.html', {'name':nome, 'dia': boleano})
+
+#def saudacao(request, nome):
+    #return HttpResponse(f"<h1 style='color: red'>Olá {nome}</h1>")
